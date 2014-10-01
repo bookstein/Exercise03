18c7c4976414f385cb5363c2bb4992e8d8fb8047
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 16:26:54 -0700

    Updated arithmetic.py to accept variable arguments.

--- arithmetic.py
+++ arithmetic.py
@@ -24,34 +24,54 @@
 # mod(int, int) -> int
 # Returns the remainder when the first integer is divided by the second integer.
 
-def add(num1, num2):
+def add(*args):
     """Returns the sum of the two input integers"""
-    return num1 + num2
+    total = 0
+    for a in args:
+        total = total + a
+    return total
 
-def subtract(num1, num2):
+def subtract(*args):
     """Returns the second number subtracted from the first"""
-    return num1 - num2
+    total = args[0]
+    print args
+    for a in args[1:]:
+        total = total - a
+        print total
+    return total
 
-def multiply(num1, num2):
+def multiply(*args):
     """Multiplies the two inputs together"""
-    return num1 * num2
+    total = 1
+    for a in args:
+        total = total * a
+    return total
 
-def divide(num1, num2):
+def divide(*args):
     """Divides the first input by the second, returning a floating point"""
-    return float(num1) / float(num2)
+    total = args[0]
+    for a in args[1:]:
+        total = total / a
+    return total
 
 def square(num1):
     """Returns the square of the input"""
-    return num1 * num1
+    return num1**2
 
 def cube(num1):
     """ Returns the cube of the input"""
     return num1**3
 
-def power(num1, num2):
+def power(*args):
     """Raises the first integer to the power of the second integer and returns the value"""
-    return num1**num2
+    total = args[0]
+    for a in args[1:]:
+        total = total ** a
+    return total
 
-def mod(num1, num2):
+def mod(*args):
     """Returns the remainder when the first integer is divided by the second integer."""
-    return num1 % num2
+    total = args[0]
+    for a in args[1:]:
+        total = total % a
+    return total
