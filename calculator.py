9c98559b9e72626f7dd11a57f46be974755e8f9d
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 15:05:28 -0700

    Checks that the inputs are numbers

--- calculator.py
+++ calculator.py
@@ -7,18 +7,17 @@ calculator program yourself in this file.
 import arithmetic
 
 def main():
-    # Your code goes here
-    
-    # No setup
-    # repeat forever:
+
     while True:
-    #     read input
         input = raw_input("> ")
-        #     tokenize input
         tokens = input.split(" ")
         if tokens[0] == 'q':
             break
-        cmd, number1, number2 = tokens[0], int(tokens[1]), int(tokens[2])
+        if tokens[1].isdigit() and tokens[2].isdigit():
+            cmd, number1, number2 = tokens[0], int(tokens[1]), int(tokens[2])
+        else:
+            print "This calculator can only calculate numbers."
+            continue
         if cmd == '+':
             print arithmetic.add(number1, number2)
         elif cmd == '-':
@@ -38,9 +37,6 @@ def main():
         else:
             print "I don't understand"
 
-    #     if the first token is 'q', quit
-    #     otherwise decide which math function to call based on the tokens we read
-
-
+   
 if __name__ == '__main__':
     main()
